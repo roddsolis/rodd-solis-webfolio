3bc836ce16239b79158d444abdac65292d91a64c
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-10-06 00:11:58 -0300

    agregue la estructura básica para mi pagina web

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const App = () => {
-  return <div>hola mundo</div>;
+  return (
+    <>
+      <nav>
+        <div className="logoContainer">logo</div>
+        <div className="statusContainer">disponible</div>
+        <ul>
+          <li>Sobre mi</li>
+          <li>Experiencia</li>
+          <li>Habilidades</li>
+          <li>Proyectos</li>
+          <li>Contacto</li>
+        </ul>
+        <div className="ctaContainer">
+          <button>hablemos</button>
+        </div>
+      </nav>
+      <header>acá ha la cabecera</header>
+      <footer>footer</footer>
+    </>
+  );
 };
 
 export default App;
