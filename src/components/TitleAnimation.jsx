83bb7214c90f7059037dd0f87e87334b8b4135de
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-10-12 18:47:33 -0300

    avances en el texto animado principal y en la aplicacion de efectos

--- src/components/TitleAnimation.jsx
+++ src/components/TitleAnimation.jsx
@@ -4,7 +4,11 @@ const TitleAnimation = ({ text1, text2, text3 }) => {
   return (
     <>
       <div className="text-animation">
-        {text1}-{text2}-{text3}
+        <p>{text1}</p>
+        <p className="guion-1">-</p>
+        <p>{text2}</p>
+        <p className="guion-2">-</p>
+        <p>{text3}</p>
       </div>
     </>
   );
